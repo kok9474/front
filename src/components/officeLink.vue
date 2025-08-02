64cb41da8bd9ec249bba1d76f65ffaaa223051a3
jiwonchoi924 <lucid_2363@naver.com>
2025-08-03 01:43:27 +0900

    main 퍼블리싱

---
+++ src/components/officeLink.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <section class="section">
+        <h2>관련부서 연결</h2>
+        <button class="submit-btn">접수하기 ➔</button>
+        <div class="card-container">
+            <div class="card">가로등 빨리 고쳐주세요<br><small>해당 지역 가로등이 점등되지 않아...</small></div>
+            <div class="card">가로등 빨리 고쳐주세요<br><small>해당 지역 가로등이 점등되지 않아...</small></div>
+            <div class="card">가로등 빨리 고쳐주세요<br><small>해당 지역 가로등이 점등되지 않아...</small></div>
+        </div>
+    </section>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
