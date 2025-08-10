641b92851387ee140ee226e74736779b0bbdb799
kok9474 <kok947412@gmail.com>
2025-08-10 22:38:57 +0900

    asd

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+</script>
+
+<template>
+  <HomeView />
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
